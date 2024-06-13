77e15416183caf4965435d9c44eaf4c3d0738232
ibkumar <ibkumarandroid@gmail.com>
2024-06-13 13:06:35 +0530

    Getting suprise me question randomly from string array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import { useState } from "react";
 
-const  App = () => {
+const App = () => {
 
   const [image, setImage] = useState(null)
+  const [value, setValue] = useState("")
+  const [response,setResponse] = useState("")
+  const [error,setError] = useState("")
+
+  const surpriseOptions = [
+    'Does the image have a whale?',
+    'Is the image fabulously pin?',
+    'Does the image have puppies?'
+  ]
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    console.log(randomValue)
+    setValue(randomValue)
+  }
 
   const uploadImage = async (e) => {
     //console.log(e.target.files)
@@ -10,24 +25,68 @@ const  App = () => {
     formData.append('file', e.target.files[0])
     setImage(e.target.files[0])
     e.target.value = null
-    try{
+    try {
       const options = {
-        method:'POST',
+        method: 'POST',
         body: formData
       }
       const response = await fetch('http://localhost:8000/upload', options)
       const data = response.json()
       console.log(data)
+    } catch (err) {
+      setError("Something didn't work please try again.");
+    }
+  }
+  const analyzeImage = () => {
+    if(!image){
+      setError("Error! Must have an existing image!")
+      return
+    }
+    try{
+
     }catch(err){
       console.log(err)
+      setError("Something didn't work please try again.");
     }
+
+  }
+  const clear = () => {
+    setImage(null)
+    setValue("")
+    setResponse("")
+    setError("")
   }
 
   return (
     <div className="app">
-      {image && <img src={URL.createObjectURL(image)} alt="images"/> }
-      <label htmlFor="files">upload an image</label>
-    <input onChange={uploadImage} id="files" accept="image/*" type="file" hidden/>
+      <section className="section-container">
+        <div className="image-container">
+          {image && <img src={URL.createObjectURL(image)} alt="images" />}
+        </div>
+      
+      <p className="extra-info">
+        <span>
+          <label htmlFor="files">upload an image</label>
+          <input onChange={uploadImage} id="files" accept="image/*" type="file" hidden />
+        </span>
+        To ask question about.
+      </p>
+      <p>
+        What do you know about the image?
+        <button className="surprise" onClick={surprise} disabled={response}>Surprise me</button>
+      </p>
+      <div className="input-container">
+        <input 
+        value={value}
+        placeholder="What is in the image..."
+        onChange = {e => setValue(e.target.value)}
+        />
+        {(!response && !error) && <button onClick={analyzeImage}>ASk me</button>}
+        {(response && error) && <button onClick={clear}>Clear</button>}
+      </div>
+      {error && <p>{error}</p>}
+      {response && <p className="answer">{response}</p>}
+      </section>
     </div>
   );
 }
